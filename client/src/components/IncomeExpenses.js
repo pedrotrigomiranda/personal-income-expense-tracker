5558bbe81278f92c7992f6e1aeaca25789bd7308
pedrotrigomiranda <pedrotrigomiranda@gmail.com>
2020-02-28 15:36:38 +0000

    added numbers format and build

--- client/src/components/IncomeExpenses.js
+++ client/src/components/IncomeExpenses.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
+import { numberWithCommas } from "../utils/format";
 
 export const IncomeExpenses = () => {
   const { transactions } = useContext(GlobalContext);
@@ -12,19 +13,19 @@ export const IncomeExpenses = () => {
     .toFixed(2);
 
   const expense = (
-    amounts.filter(value => value < 0)
-           .reduce((acc, curr) => (acc += curr), 0) * -1)
-           .toFixed(2);
+    amounts.filter(value => value < 0).reduce((acc, curr) => (acc += curr), 0) *
+    -1
+  ).toFixed(2);
 
   return (
     <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
-        <p className="money plus">{income} €</p>
+        <p className="money plus">{numberWithCommas(income)} €</p>
       </div>
       <div>
         <h4>Expense</h4>
-        <p className="money minus">{expense} €</p>
+        <p className="money minus">{numberWithCommas(expense)} €</p>
       </div>
     </div>
   );
--- client/src/components/Transaction.js
+++ client/src/components/Transaction.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
+import { numberWithCommas } from "../utils/format";
 
 export const Transaction = ({ transaction }) => {
   const { deleteTransaction } = useContext(GlobalContext);
@@ -11,11 +12,11 @@ export const Transaction = ({ transaction }) => {
       {transaction.text}{" "}
       <span>
         {sign}
-        {Math.abs(transaction.amount)} €
+        {numberWithCommas(Math.abs(transaction.amount))} €
       </span>
       <button
         className="delete-btn"
-        onClick={() => deleteTransaction(transaction.id)}
+        onClick={() => deleteTransaction(transaction._id)}
       >
         x
       </button>
